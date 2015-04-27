804831e365a86f8f625e3df2bf4a2d6ff86e32b0
Cubarco <marco.l.here@gmail.com>
2015-04-27 09:41:04 +0800

    Add dl_dst to matches

--- 2.1/2_1.py
+++ 2.1/2_1.py
@@ -11,6 +11,7 @@ class RestrictedHTTPRequest(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(RestrictedHTTPRequest, self).__init__(*args, **kwargs)
         self.h2_mac = '00:00:00:00:00:02'
+        self.h4_mac = '00:00:00:00:00:04'
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_feature_handler(self, ev):
@@ -18,10 +19,15 @@ class RestrictedHTTPRequest(app_manager.RyuApp):
         parser = datapath.ofproto_parser
         dpid = datapath.id
 
-        print 'Switch s%d attached.' % dpid
+        print 'Switch s%d connected.' % dpid
         if dpid == 3:
-            match = parser.OFPMatch(in_port=1, eth_src=self.h2_mac)
+            match = parser.OFPMatch(in_port=1,
+                                    eth_src=self.h2_mac, eth_dst=self.h4_mac)
             actions = []  # Drop
+            # Flow entry:
+            # table=0, priority=65535,in_port=1,dl_src=00:00:00:00:00:02,
+            # dl_dst=00:00:00:00:00:04
+            # actions=drop
             self.add_flow(datapath, 65535, match, actions)
 
     def add_flow(self, datapath, priority, match, actions, hard_timeout=None):
