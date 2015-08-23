41c19504fe5ff16c2cc9034f980f6df132098132
Cubarco <marco.l.here@gmail.com>
2015-08-23 15:33:17 +0800

    cache server implemented partly

--- final/cpu_example_func.py
+++ final/cpu_example_func.py
@@ -3,9 +3,13 @@
 
 import socket
 
-s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW)
+s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
 # From the docs: "For raw packet
 # sockets the address is a tuple (ifname, proto [,pkttype [,hatype]])"
 s.bind(('h1-eth0', 0))
-s.send('\x00' * 6 + '\xFF' * 6 + '\x99\x99' +
-       '{"func_id": 1234, "desc": "This is an example description."}')
+while True:
+    print(', '.join(map(lambda x: hex(ord(x)), s.recv(2048))))
+    done = 1
+    if not done:
+        s.send('\x00' * 6 + '\xFF' * 6 + '\x99\x99' +
+               '{"func_id": 1234, "desc": "This is an example description."}')
